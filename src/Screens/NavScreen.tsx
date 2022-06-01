d69f7efc30491284f1bde3ab5b289956bb6080d0
eter19 <claudita.et@gmail.com>
2022-06-01 10:44:24 -0400

    Se realizo las importaciones de las librerias y creacion de navbar

---
+++ src/Screens/NavScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const NavScreen = () => {
+  return (
+    <div className='container'>
+        <ul>
+          <li>
+            Inicio
+          </li>
+          <li>
+            Presentacion
+          </li>
+          <li>
+            Tecnologias
+          </li>
+          <li>
+            Proyectos
+          </li>
+          <li>
+            Contacto
+          </li>
+        </ul>
+    </div>
+  )
+}
