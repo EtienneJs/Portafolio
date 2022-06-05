b647647306f380113f3e9dd11c0330e34d3d3ac6
eter19 <claudita.et@gmail.com>
2022-06-05 19:16:01 -0400

    Se configuro el menu para que este activado

--- src/Screens/NavScreen.js
+++ src/Screens/NavScreen.js
@@ -1,12 +1,6 @@
 import  {  useState } from 'react'
 import '../Styles/navbar_styles.css'
-const initialStateBtnActive = {
-  inicio: false,
-  presentacion:false,
-  tecnologias:false,
-  proyectos:false,
-  contactos:false
-}  
+
 
 
 export const NavScreen = ({isVisibleInicio, isVisiblePresent, isVisibleTecnology,isVisibleProyect,isVisibleContac}) => {
@@ -16,14 +10,6 @@ export const NavScreen = ({isVisibleInicio, isVisiblePresent, isVisibleTecnology
     btnClose:false,
     btnOpen:true
   })
-  const [btnActive, setBtnActive] = useState(initialStateBtnActive)
-  const handleBtnActive = (e) =>{
-    const menuItem = e.currentTarget.text.toLowerCase()
-    setBtnActive({
-      ...initialStateBtnActive,
-      [menuItem]: true
-    })
-  }
   const handleMenu =()=>{
 
     if(menu.btnOpen === true){
@@ -38,9 +24,6 @@ export const NavScreen = ({isVisibleInicio, isVisiblePresent, isVisibleTecnology
         btnClose:false,
         btnOpen:true
       })
-      setBtnActive({
-        ...initialStateBtnActive
-      })
     }
   } 
   console.log(isVisibleContac)
@@ -50,19 +33,19 @@ export const NavScreen = ({isVisibleInicio, isVisiblePresent, isVisibleTecnology
         <div className='d-flex justify-content-start align-items-center navbar_ul'>
         <i onClick={handleMenu} className={menu.btnOpen ? "fa-solid fa-bars" : "classNoc"}></i>
           <div  className={menu.menuOpen ? "d-flex flex-column menuC" : "classNoc"}>
-          <a className={ isVisibleInicio ? 'btnActive normal': 'normal'  } onClick={handleBtnActive}  href='#inicio'>
+          <a className={ isVisibleInicio ? 'btnActive normal': 'normal'  }   href='#inicio'>
             Inicio
           </a>
-          <a className={ isVisibleInicio  ? ' normal': isVisiblePresent ? ' btnActive normal':'normal'} onClick={handleBtnActive}  href='#present'>
+          <a className={ isVisibleInicio  ? ' normal': isVisiblePresent ? ' btnActive normal':'normal'}   href='#present'>
             Presentacion
           </a>
-          <a  className={ isVisibleInicio  ? ' normal':isVisiblePresent ? 'normal':isVisibleTecnology ? ' btnActive normal':'normal'} onClick={handleBtnActive}  href='#tec'>
+          <a  className={ isVisibleInicio  ? ' normal':isVisiblePresent ? 'normal':isVisibleTecnology ? ' btnActive normal':'normal'}   href='#tec'>
             Tecnologias
           </a>
-          <a  className={isVisibleTecnology ? 'normal': isVisibleContac ? 'normal':isVisibleProyect ? 'btnActive normal':'normal'} onClick={handleBtnActive} href='#proyect'>
+          <a  className={isVisibleTecnology ? 'normal': isVisibleContac ? 'normal':isVisibleProyect ? 'btnActive normal':'normal'}  href='#proyect'>
             Proyectos
           </a>
-          <a  className={ isVisibleContac ? 'btnActive normal':'normal'} onClick={handleBtnActive} href='#contac'>
+          <a  className={ isVisibleContac ? 'btnActive normal':'normal'}  href='#contac'>
             Contactos
           </a>
           </div>
