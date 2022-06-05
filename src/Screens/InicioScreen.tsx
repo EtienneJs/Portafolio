c3d7340c27fc32f518687d8c1f2121f3d491331e
eter19 <claudita.et@gmail.com>
2022-06-04 22:41:05 -0400

    Se arreglo las letras del InicioScreen

--- src/Screens/InicioScreen.tsx
+++ src/Screens/InicioScreen.tsx
@@ -5,9 +5,9 @@ export const InicioScreen = () => {
   return (
     <div id='inicio' className='BackImg'>
        <h1>
-       <span className='content1'>Etienne Fernandez</span>
-        <span className='content2'>Desarrollador Web FullStack</span>
-        <span className='content3'>MERN</span>
+       <span className='content1 '>Etienne Fernandez</span>
+        <span className='content2 '>Desarrollador Web FullStack</span>
+        <span className='content3 '>MERN</span>
        </h1>
     </div>
   )
